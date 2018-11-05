43233b64c929b655013a20d50fd102d02411444e
xys20071111 <xys20071111@163.com>
2018-11-05 14:43:00 +0800

    待我查一下舰C的Pixi.js的版本号

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 "use strict"
 const express = require('express');
 const fs =require('fs');
-const httpsServer = require('httpsServer');
+const https = require('https');
 const options = {
 	    key:fs.readFileSync('server.key'),
 	    cert:fs.readFileSync('server.crt')
 }
 const app = express();
-app.use('',function(req,res){});
+app.use('/',function(req,res){
+	res.set('Content-Type','application/json');
+	res.send(fs.readFileSync('./js/pixijs/pixi.min.js'));
+});
+https.createServer(options,app).listen(8081)
