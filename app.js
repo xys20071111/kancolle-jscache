c5e7e84a1d29088d5d3f389232cab85c6d45e887
xys20071111 <xys20071111@163.com>
2018-11-14 18:41:51 +0800

    准备用上路由和url

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs =require('fs');
 const https = require('https');
 const log = require('./logger');
+const url = require('url');
 const options = {
 	    key:fs.readFileSync('server.key'),
 	    cert:fs.readFileSync('server.crt')
